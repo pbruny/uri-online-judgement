cfad39ac81a188ba38e18e70d8276ad7ef233e79
Paulo Lima <paulobruny@gmail.com>
2018-04-17 23:12:41 -0300

    Add solution to 1013 problem

---
+++ 1013.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int A, B, C, maiorAB;
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    maiorAB = (A + B + abs(A-B))/2;
+    maiorAB = (maiorAB + C + abs(maiorAB - C))/2;
+
+    printf("%d eh o maior\n", maiorAB);
+
+    return 0;
+}
